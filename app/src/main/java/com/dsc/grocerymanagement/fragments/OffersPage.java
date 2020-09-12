133849fbfe9182ac5141a83afefbd26d7c70d5e6
KalpanaSinha <1805299@kiit.ac.in>
2020-09-12 10:45:10 +0530

    Displaying OfferPage and OfferDetails Completed

--- app/src/main/java/com/dsc/grocerymanagement/fragments/OffersPage.java
+++ app/src/main/java/com/dsc/grocerymanagement/fragments/OffersPage.java
@@ -1,15 +1,29 @@
 package com.dsc.grocerymanagement.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.dsc.grocerymanagement.R;
+import com.dsc.grocerymanagement.activities.OfferActivity;
+import com.dsc.grocerymanagement.model.offermodel;
 import com.dsc.grocerymanagement.util.IOnBackPressed;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +31,11 @@ import com.dsc.grocerymanagement.util.IOnBackPressed;
  * create an instance of this fragment.
  */
 public class OffersPage extends Fragment implements IOnBackPressed {
+
+    RecyclerView recview;
+    FirebaseFirestore firebaseFirestore;
+    FirestoreRecyclerAdapter adapter;
+    RelativeLayout offerLayout;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,8 +81,108 @@ public class OffersPage extends Fragment implements IOnBackPressed {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_offers_page, container, false);
+        View view= inflater.inflate(R.layout.fragment_offeritem_page, container, false);
+
+
+
+        recview=(RecyclerView)view.findViewById(R.id.firestore_list);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        offerLayout = view.findViewById(R.id.progressLayout);
+        offerLayout.setVisibility(View.VISIBLE);
+
+
+        Query query = FirebaseFirestore.getInstance()
+                .collection("offers");
+
+
+        getList(query);
+
+        return view;
     }
+
+    private void getList(Query query){
+        FirestoreRecyclerOptions<offermodel> options = new FirestoreRecyclerOptions.Builder<offermodel>()
+                .setQuery(query, offermodel.class)
+                .build();
+
+        adapter = new FirestoreRecyclerAdapter<offermodel, OfferViewHolder>(options){
+
+
+            @NonNull
+            @Override
+            public OfferViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View viewoffer = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_offers_page, parent, false);
+                return new OfferViewHolder(viewoffer);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull OfferViewHolder holder, int position, @NonNull final offermodel model) {
+
+                holder.offername.setText(model.getOffername());
+                holder.offertype.setText(model.getOffertype());
+                Glide.with(holder.offerimg.getContext()).load(model.getOfferimg()).into(holder.offerimg);
+                holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(getContext(), OfferActivity.class);
+                        intent.putExtra("offerdesc",model.getOfferdesc());
+                        intent.putExtra("c1",model.getC1());
+                        intent.putExtra("c2",model.getC2());
+                        intent.putExtra("c3",model.getC3());
+
+                        intent.putExtra("offerimage",model.getOfferimage().toString());
+                        view.getContext().startActivity(intent);
+                    }
+                });
+                offerLayout.setVisibility(View.GONE);
+
+
+            }
+        };
+
+        // adapter.notifyDataSetChanged();
+        recview.setHasFixedSize(true);
+        recview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recview.setAdapter(adapter);
+
+    }
+
+
+
+    private class OfferViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView offerimg;
+        TextView offername,offertype;
+        offermodel model;
+        RelativeLayout relativeLayout;
+
+
+        public OfferViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            offerimg = (ImageView) itemView.findViewById(R.id.offerimage);
+            offername = (TextView) itemView.findViewById(R.id.offername);
+            offertype = (TextView) itemView.findViewById(R.id.offertype);
+            relativeLayout=itemView.findViewById(R.id.offerlayout);
+
+
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
     @Override
     public boolean onBackPressed() {
         return false;
---
+++ app/src/main/java/com/dsc/grocerymanagement/model/offermodel.java
@@ -0,0 +1,84 @@
+package com.dsc.grocerymanagement.model;
+
+
+public class offermodel {
+    String offername;
+    String offerimg;
+    String offertype;
+    String c1,offerdesc,offerimage,c2,c3;
+
+    public offermodel() {
+
+    }
+
+    public offermodel(String offername, String offerimg, String offertype) {
+        this.offername=offername;
+        this.offerimg=offerimg;
+        this.offertype=offertype;
+    }
+
+    public String getC1() {
+        return c1;
+    }
+
+    public void setC1(String c1) {
+        this.c1 = c1;
+    }
+
+    public String getC2() {
+        return c2;
+    }
+
+    public void setC2(String c2) {
+        this.c2 = c2;
+    }
+
+    public String getC3() {
+        return c3;
+    }
+
+    public void setC3(String c3) {
+        this.c3 = c3;
+    }
+
+
+    public String getOfferdesc() {
+        return offerdesc;
+    }
+
+    public void setOfferdesc(String offerdesc) {
+        this.offerdesc = offerdesc;
+    }
+
+    public String getOfferimage() {
+        return offerimage;
+    }
+
+    public void setOfferimage(String offerimage) {
+        this.offerimage = offerimage;
+    }
+
+    public String getOffername() {
+        return offername;
+    }
+
+    public void setOffername(String offername) {
+        this.offername = offername;
+    }
+
+    public String getOfferimg() {
+        return offerimg;
+    }
+
+    public void setOfferimg(String offerimg) {
+        this.offerimg = offerimg;
+    }
+
+    public String getOffertype() {
+        return offertype;
+    }
+
+    public void setOffertype(String offertype) {
+        this.offertype = offertype;
+    }
+}
